412fd9e78a4c1978f962b390e1f77bfc3fe611d0
Hafiz1110 <mhagbut@gmail.com>
2026-06-08 13:38:32 +0700

    Your changes

--- script/search.js
+++ script/search.js
@@ -1,10 +1,1 @@
 
-let seachInput = document.getElementById("search");
-seachInput.addEventListener("input", function() {
-    let searchTerm = seachInput.value.toLowerCase();
-    let items = document.querySelectorAll(".item");
-    items.forEach(function(item) {        let itemName = item.querySelector(".item-name").textContent.toLowerCase();
-        if (itemName.includes(searchTerm)) {
-            item.style.display = "block";     } else {
-            item.style.display = "none";      }
-    });});
